e2be0e395a975145ecb993769c9eba2cb5f353c8
Md. Nayem Khan <nayemk900@gmail.com>
2022-04-13 16:06:43 +0600

    updated design

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,24 +7,7 @@ import Layout from "../components/Layout/Layout";
 const IndexPage = () => {
   return (
     <Layout>
-      {/* <HeroSection/> */}
-      {/* <About/> */}
-      {/* <Milestone/> */}
-      {/* <Contributions/> */}
-      {/* <Gallery/> */}
-      {/* <Contact/> */}
-      {/* <div className="flex p-10 justify-between">
-        <div className="p-24">
-          <p className="text-xs text-animationTitle font-normal">
-            Hello, I am
-          </p>
-          <p className="text-heroTitle text-lg">Tanvir Hossain Khan</p>
-          <p className="text-uppercase text-heroTitle">Entrepreneur</p>
-        </div>
-        <div className="w-2/6">
-          <img src="https://cdn.pixabay.com/photo/2014/06/03/19/38/road-sign-361514_960_720.png" alt="" className="w-full" />
-        </div>
-      </div> */}
+      
     </Layout>
   );
 };
