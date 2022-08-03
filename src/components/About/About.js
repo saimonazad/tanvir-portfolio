06ee99590ef5665ce4e375124dc4645454f31c0a
Nayem Khan <nayemk900@gmail.com>
2022-08-03 14:14:40 +0600

    updated text font

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,7 +15,7 @@ const About = () => {
   return (
     <div className="text-center mt-about md:mt-0">
       <div className="xl:pt-20 xl:pb-5 pt-24 2xl:pt-48 2xl:pb-10 text-center">
-        <p className="text-aboutTitle tracking-custom text-2xl sm:text-4xl 2xl:text-7xl md:text-5xl lg:text-3xl font-impact font-black">Tanvir Hossain Khan</p>
+        <p className="text-aboutTitle tracking-custom text-2xl sm:text-4xl 2xl:text-6xl md:text-5xl lg:text-3xl font-impact font-black">Tanvir Hossain Khan</p>
       </div>
       <div className="bg-white rounded-t-aboutBorderRadius lg:pb-16 2xl:pb-about py-10 2xl:px-40 sm:px-10 shadow-lg">
             <p className="text-aboutSubtitle tracking-widest md:text-xl text-xs lg:text-xl 2xl:text-2xl font-PT_Serif font-bold uppercase xl:py-5 md:py-8 my-5">
